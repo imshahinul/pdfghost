410acd14eeb57fa555f2dbf0f7ca25e01093bec9
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2025-02-09 00:41:08 -0500

    Add functionality for splitting pdf and expose them in __init__.py

--- pdfghost/__init__.py
+++ pdfghost/__init__.py
@@ -1,5 +1,7 @@
 from .functions.merger import merge_pdfs
+from .functions.splitter import split_pdf
 
 __all__ = [
     "merge_pdfs",
+    "split_pdf",
 ]
--- pdfghost/functions/splitter.py
+++ pdfghost/functions/splitter.py
@@ -11,3 +11,18 @@ def split_pdf(input_path, output_folder, split_range=None):
     :param split_range: Tuple (start_page, end_page) to split. If None, splits all pages.
     :raises FileNotFoundError: If the input file does not exist.
     """
+    validate_file_path(input_path)
+    validate_directory_path(output_folder)
+
+    reader = PdfReader(input_path)
+    writer = PdfWriter()
+
+    start_page = split_range[0] if split_range else 0
+    end_page = split_range[1] if split_range else len(reader.pages)
+
+    for i in range(start_page, end_page):
+        writer.add_page(reader.pages[i])
+
+    output_path = f"{output_folder}/split_{start_page + 1}_to_{end_page}.pdf"
+    with open(output_path, "wb") as output_pdf:
+        writer.write(output_pdf)
