95d486adc999a712bf7005b40e885067cd7b96ac
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2025-02-16 03:10:40 -0500

    Add test case for encrypting and decrypting pdf files and initial code structure

---
+++ pdfghost/functions/encryption.py
@@ -0,0 +1,24 @@
+# pdfghost/functions/encryption.py
+
+
+def encrypt_pdf(input_path, output_path, password):
+    """
+    Encrypt a PDF with a password.
+
+    :param input_path: Path to the input PDF.
+    :param output_path: Path to save the encrypted PDF.
+    :param password: Password to encrypt the PDF.
+    :raises FileNotFoundError: If the input file does not exist.
+    """
+
+
+def decrypt_pdf(input_path, output_path, password):
+    """
+    Decrypt a PDF with a password.
+
+    :param input_path: Path to the input PDF.
+    :param output_path: Path to save the decrypted PDF.
+    :param password: Password to decrypt the PDF.
+    :raises FileNotFoundError: If the input file does not exist.
+    :raises ValueError: If the password is incorrect.
+    """
